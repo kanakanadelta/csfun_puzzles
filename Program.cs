c571e5a6d9a81f636b00f9e7e99afa64b20e51f2
Rafael Tongson <rtongson@peppymiint.local>
2019-10-14 17:51:51 -0700

    write names function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Puzzles
 {
@@ -36,16 +37,59 @@ namespace Puzzles
             Random rand = new Random();
             Console.WriteLine("Tossing a coin...");
             int result = rand.Next(0,2);
-            if(result == 0) {
+            if(result == 0) 
+            {
                 Console.WriteLine($"Tails, {result}");
                 return "Tails";
-            } else {
+            } else 
+            {
                 Console.WriteLine($"Heads, {result}");
                 return "Heads";
             }
         }
 
+        public static double TossMultipleCoins(int num)
+        {
+            int wins = 0;
+            int tossCount = 0;
+            Console.WriteLine("Multiple Coin Toss, start");
+            while(tossCount < num)
+            {
+                string coinToss = TossCoin();
+                if(coinToss == "Heads")
+                {
+                    Console.WriteLine("you won a toss!");
+                    wins++;
+                }
+                tossCount++;
+            }
+            double winRatio = (double)wins/num;
+            Console.WriteLine($"wins ratio at: {winRatio*100}/100");
+            return winRatio*100;
+        }
+        public static List<string> Names(){
+            List<string> names = new List<string>();
+            names.Add("Todd");
+            names.Add("Tiffany");
+            names.Add("Charlie");
+            names.Add("Geneva");
+            names.Add("Sydney");
 
+            Random rand = new Random();
+            int idx = 0;
+
+            for(int currIdx = idx; currIdx < names.Count; currIdx++) {
+                string temp = names[currIdx];
+                int rIdx = rand.Next(currIdx, names.Count);
+                names[currIdx] = names[rIdx];
+                names[rIdx] = temp;
+                idx++;
+            }
+
+            string namesStr = string.Join(",", names);
+            Console.WriteLine(namesStr);
+            return names;
+        }
         // // // //
         // MAIN //
         static void Main(string[] args)
@@ -53,6 +97,8 @@ namespace Puzzles
             Console.WriteLine("Puzzles Practice");
             RandomArray();
             TossCoin();
+            TossMultipleCoins(10);
+            Names();
         }
     }
 }
